cf3b81508c0048c6e568f6a0e0b9b2d78904ad39
Aniket Khanchi <108761529+aniket-khanchi@users.noreply.github.com>
2023-01-21 19:10:11 +0000

    bkg remove completion

--- bkg_remove.py
+++ bkg_remove.py
@@ -2,4 +2,19 @@ from rembg import remove
 from PIL import Image
 
 
-print('sky_bad')
+import os
+
+directory = '/workspaces/megametals/assets/img/Water'
+count = 1
+for filename in os.listdir(directory):
+    if filename.endswith('.jpg'):
+        filepath = os.path.join(directory, filename)
+        # print(filepath)
+            # do something with the content of the file here
+
+# input_path = '/workspaces/megametals/assets/img/Agriculture/IMG_9484.jpg'
+        output_path = '/workspaces/megametals/Products/Water/' + str(count) + '.png'
+        input = Image.open(filepath)
+        output = remove(input)
+        output.save (output_path)
+        count +=1
